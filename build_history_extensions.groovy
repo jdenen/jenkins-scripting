c9f313f0ce9ba4670650bd5dbfb1b3230de296b7
Johnson Denen <jdenen@manta.com>
2013-12-19 11:50:10 -0500

    fix duration

--- build_history_extensions.groovy
+++ build_history_extensions.groovy
@@ -7,10 +7,9 @@
 // Requires the htmlpublisher plugin archiving a report called 'Results'.
 //
 
-//def dtime = ( manager.build.getDurationString() =~ /(\d+*)/ )
 def regex = ( manager.build.project =~ /\[(.*)\]/ )
 def base = "${ manager.hudson.getRootUrl() }job/${ regex[0][1] }/${ manager.build.number }/Results"
 
-//manager.addBadge("/images/16x16/hourglass.gif", "Duration: ~${ dtime[0][1] } seconds")
+manager.addBadge("/images/16x16/hourglass.gif", "Duration: ${ manager.getLogMatcher(".*Finished in (.*)\$").group(1) }")
 manager.addBadge("text.gif", "HTML Report", "$base")
 manager.addBadge("info.gif", "Build SHAs", "$base/stack_file.txt")
