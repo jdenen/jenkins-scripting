e399a4f8af106fb576bf5def776177ddf60a2f00
Johnson Denen <jdenen@manta.com>
2013-12-19 12:16:18 -0500

    refine script

--- unexpected_response.groovy
+++
@@ -1,7 +0,0 @@
-// 
-// unstable build caused by unexpected response
-//
-if(manager.logContains('.*unexpected response, code=404, content-type="text/plain".*')) {
-  manager.addWarningBadge("unexpected response (PhantomJS error)");
-  manager.build.@result = hudson.model.Result.UNSTABLE
-}
