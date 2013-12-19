a7e319ea05440162c15cd884f45408ef9cd3e420
Johnson Denen <jdenen@manta.com>
2013-12-19 11:21:21 -0500

    generalize the base url

--- build_history_extensions.groovy
+++ build_history_extensions.groovy
@@ -1,6 +1,15 @@
+//
+// Adds three icons to the Build History widget for each build.
+//   1. Clock icon with duration tooltip
+//   2. Document icon with link to HTML report
+//   3. Info icon with link to environment SHA information
+//
+// Requires the htmlpublisher plugin archiving a report called 'Results'.
+//
+
 def dtime = ( manager.build.getDurationString() =~ /(\d+)/ )
 def regex = ( manager.build.project =~ /\[(.*)\]/ )
-def base = "http://jenkins.aws.ecnext.net:8080/job/${ regex[0][1] }/${ manager.build.number }/Results"
+def base = "${ manager.hudson.getRootUrl() }:8080/job/${ regex[0][1] }/${ manager.build.number }/Results"
 
 manager.addBadge("/images/16x16/hourglass.gif", "Duration: ~${ dtime[0][1] } seconds")
 manager.addBadge("text.gif", "HTML Report", "$base")
