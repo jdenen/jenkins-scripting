268900b6cb09daaef94c89ce495a5a51294023ee
Johnson Denen <jdenen@manta.com>
2013-12-19 11:24:44 -0500

    quick fix

--- build_history_extensions.groovy
+++ build_history_extensions.groovy
@@ -9,7 +9,7 @@
 
 def dtime = ( manager.build.getDurationString() =~ /(\d+)/ )
 def regex = ( manager.build.project =~ /\[(.*)\]/ )
-def base = "${ manager.hudson.getRootUrl() }:8080/job/${ regex[0][1] }/${ manager.build.number }/Results"
+def base = "${ manager.hudson.getRootUrl() }job/${ regex[0][1] }/${ manager.build.number }/Results"
 
 manager.addBadge("/images/16x16/hourglass.gif", "Duration: ~${ dtime[0][1] } seconds")
 manager.addBadge("text.gif", "HTML Report", "$base")
