f9a17f9a5aa195d815c0da77716669cf09aca558
Johnson Denen <jdenen@manta.com>
2014-01-03 15:18:42 -0500

    add unstable build script

---
+++ mark_build_unstable.groovy
@@ -0,0 +1,12 @@
+//
+// Forces build status to UNSTABLE if one of these conditions is met:
+//    1. PhantomJS produces an "unexpected response" error
+//    2. Chrome produces an "unreachable" error
+//
+
+def chrome_error = manager.logContains('.*chrome not reachable.*')
+def phantom_error = manager.logContains('.*unexpected response, code=404, content-type="text/plain".*')
+
+if (chrome_error || phantom_error) {
+  manager.build.@result = hudson.model.Result.UNSTABLE
+}
