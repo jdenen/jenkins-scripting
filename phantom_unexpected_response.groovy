15741accff152e03d1c385365fe1c31bc7fc907f
Johnson Denen <jdenen@manta.com>
2014-01-07 15:49:55 -0500

    remove duplicate script

--- phantom_unexpected_response.groovy
+++
@@ -1,8 +0,0 @@
-// 
-// Forces build status to UNSTABLE if console output includes the unexpected response error
-// that pops up occassionally when running tests with PhantomJS.
-//
-
-if (manager.logContains('.*unexpected response, code=404, content-type="text/plain".*')) {
-  manager.build.@result = hudson.model.Result.UNSTABLE
-}
